700de3d3b933df0f27ea8bfd56ec36f095e34573
samashtar <sam.ashtar@flatironschool.com>
2019-02-25 22:13:40 -0500

    customers route

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 3000,
+    URL: process.env.BASE_URL || 'http://localhost:3000',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://node-website:1234@cluster0-yuvr5.mongodb.net/test?retryWrites=true'
+}
